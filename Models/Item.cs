e14d85ce297c6bdd61fd31cf7417a14c35629963
narges-Jamshidi <iamsabwina@gmail.com>
2024-09-14 14:38:24 +0330

    item class

--- Models/Item.cs
+++ Models/Item.cs
@@ -2,4 +2,14 @@
 
 public class Item
 {
+    private string name { get; }
+    private string description { get; }
+    private double cost { get; }
+
+    public Item(string name, string description, double cost)
+    {
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+    }
 }
